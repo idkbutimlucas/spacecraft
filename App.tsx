408f9c2bad7919d142786ba66ff601d47fdad0ce
idkbutimlucas <76565606+idkbutimlucas@users.noreply.github.com>
2022-02-22 13:15:13 +0100

    fix eslint

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-// import { LoginScreen } from './src/screens/LoginScreen';
-import { StarshipFeedScreen } from './src/screens/StarshipFeedScreen';
-import { TermsScreen } from './src/screens/TermsScreen';
-import { QueryClient, QueryClientProvider } from 'react-query'
+import React from "react";
 
+// import { LoginScreen } from './src/screens/LoginScreen';
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { StarshipFeedScreen } from "./src/screens/StarshipFeedScreen";
+import { TermsScreen } from "./src/screens/TermsScreen";
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  return ( //<LoginScreen />
+  return (
+    //<LoginScreen />
     // <TermsScreen/>
     <QueryClientProvider client={queryClient}>
       <StarshipFeedScreen />
-    </QueryClientProvider>);
+    </QueryClientProvider>
+  );
 };
 
 export default App;
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,11 +1,11 @@
 const LoginScreen = () => {
-    return (
-        // using <> is just like using <React.Fragment>
-        <>
-            {/* <TextInput />
+  return (
+    // using <> is just like using <React.Fragment>
+    <>
+      {/* <TextInput />
             <TextInput />
             <Button>Login</Button> */}
-        </>
-        // using </> is just like using </React.Fragment>
-    );
+    </>
+    // using </> is just like using </React.Fragment>
+  );
 };
